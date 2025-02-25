435e29a576a8c5220a929cb0a40e31f1fa8873f6
Saurabh Vitthal Satre <saurabhsatre01@gmail.com>
2025-02-25 21:33:23 +0530

    Database model updated

--- app/layout.js
+++ app/layout.js
@@ -15,12 +15,12 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
     <html lang="en">
       <body className={`${inter.className}`}>
-        {/* header */}
+        {/* header Section */}
         <Header/>
 
         <main className="min-h-screen">{children}</main>
 
-        {/* footer */}
+        {/* footer Section */}
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto px-4 text-center text-gray-600">
             <p>Made with love</p>
